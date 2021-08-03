e7722f6a56f56b1ad2bf2a30158480fd38e15e05
Justus K <justus.k@protonmail.com>
2021-08-03 14:16:03 +0200

    Use project specific linker script instead of global one

---
+++ kernel/loader/build.rs
@@ -0,0 +1,14 @@
+use std::{env, fs, io, path::PathBuf};
+
+const LINKER_SCRIPT: &str = "../../linker-scripts/loader.ld";
+
+fn main() -> io::Result<()> {
+    // copy the kernel linker script into the output directory so the linker can find it
+    let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    fs::copy(LINKER_SCRIPT, out.join("link.ld"))?;
+
+    // tell rustc to search in the out directory for linker stuff
+    println!("cargo:rustc-link-search={}", out.display());
+    println!("cargo:rerun-if-changed={}", LINKER_SCRIPT);
+    Ok(())
+}
