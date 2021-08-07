40af80f69d9544d796db54d2ad3f879d6820585e
Valentin B <valentin.be@protonmail.com>
2021-08-07 14:57:51 +0200

    cortex-a: Initialize crate

---
+++ crates/cortex-a/src/lib.rs
@@ -0,0 +1,9 @@
+//! A library providing low-level hardware access to Cortex-A processors.
+//!
+//! This is intended as a temporary, more feature-rich replacement of the
+//! [`cortex-a`] crate until we get to upstream parts of this.
+//!
+//! [`cortex-a`]: https://crates.io/crates/cortex-a
+
+#![no_std]
+#![deny(unsafe_op_in_unsafe_fn, rustdoc::broken_intra_doc_links)]
