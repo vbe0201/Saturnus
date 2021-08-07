1d07653309d83c2f3de476346b7c3ed563bc7600
Justus K <justus.k@protonmail.com>
2021-08-07 16:15:13 +0200

    cortex-a: Link licenses and add `asm` module

---
+++ crates/cortex-a/src/asm.rs
@@ -0,0 +1,63 @@
+//! Wrappers around common ARMv8-A instructions.
+
+/// The classic no-operation instruction.
+#[inline(always)]
+pub fn nop() {
+    match () {
+        #[cfg(target_arch = "aarch64")]
+        () => unsafe { asm!("nop", options(nostack, nomem)) },
+        () => unimplemented!(),
+    }
+}
+
+/// Wait For Interrupt
+///
+/// For more details on wfi, refer to [here](http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0802a/CIHEGBBF.html).
+#[inline(always)]
+pub fn wfi() {
+    match () {
+        #[cfg(target_arch = "aarch64")]
+        () => unsafe { asm!("wfi", options(nostack, nomem)) },
+        () => unimplemented!(),
+    }
+}
+
+/// Wait For Event
+///
+/// For more details of wfe - sev pair, refer to [here](http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0802a/CIHEGBBF.html).
+#[inline(always)]
+pub fn wfe() {
+    match () {
+        #[cfg(target_arch = "aarch64")]
+        () => unsafe { asm!("wfe", options(nostack, nomem)) },
+        () => unimplemented!(),
+    }
+}
+
+/// Send EVent.Locally
+///
+/// SEV causes an event to be signaled to the local core within a multiprocessor system.
+///
+/// For more details of wfe - sev/sevl pair, refer to [here](http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0802a/CIHEGBBF.html).
+#[inline(always)]
+pub fn sevl() {
+    match () {
+        #[cfg(target_arch = "aarch64")]
+        () => unsafe { asm!("sevl", options(nostack, nomem)) },
+        () => unimplemented!(),
+    }
+}
+
+/// Send EVent.
+///
+/// SEV causes an event to be signaled to all cores within a multiprocessor system.
+///
+/// For more details of wfe - sev pair, refer to [here](http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0802a/CIHEGBBF.html).
+#[inline(always)]
+pub fn sev() {
+    match () {
+        #[cfg(target_arch = "aarch64")]
+        () => unsafe { asm!("sev", options(nostack, nomem)) },
+        () => unimplemented!(),
+    }
+}
