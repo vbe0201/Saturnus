fdc3af992f7ca914f187061db3758fbfc6b68697
Valentin B <valentin.be@protonmail.com>
2021-10-15 14:37:07 +0200

    libkern: Initialize crate

---
+++ crates/libkern/src/lib.rs
@@ -0,0 +1,10 @@
+//! A library for unifying shared kernel code.
+//!
+//! Code in this library is either:
+//!
+//! * re-used between kernel and loader simultaneously
+//!
+//! * generic enough to be usable outside of the kernel itself.
+
+#![no_std]
+#![deny(unsafe_op_in_unsafe_fn, rustdoc::broken_intra_doc_links)]
