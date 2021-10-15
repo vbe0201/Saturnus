5fec94034899adc324de8f70a3d9a64395508d27
Valentin B <valentin.be@protonmail.com>
2021-10-15 19:11:59 +0200

    libkern: Flesh out InitialPageAllocator structure

--- crates/libkern/src/_arch/aarch64/init/paging/mod.rs
+++ crates/libkern/src/_arch/aarch64/init/paging/mod.rs
@@ -1,3 +1,16 @@
-pub struct InitialPageAllocator;
+struct FreePageList {
+    head: Option<&'static mut FreePageFrame>,
+}
+
+struct FreePageFrame {
+    next: Option<&'static mut FreePageFrame>,
+    size: usize,
+}
+
+pub struct InitialPageAllocator {
+    start_address: usize,
+    next_free_address: usize,
+    page_list: FreePageList,
+}
 
 pub struct InitialPageTable;
